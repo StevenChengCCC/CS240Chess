5dd69ed5c4789c02d1013b46bc18c83f8cb0f08e
StevenChengCCC <chengssh@byu.edu>
2025-03-05 13:14:43 -0700

    register user

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,38 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
+// service for register login logout
+
 public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData register(String username, String password, String email)
+            throws DataAccessException {
+        // check if the password and username is null
+        if (username == null || password == null) {
+            throw new DataAccessException("bad request: username or password is blank");
+        }
+        // Create the user
+        var newUser = new UserData(username, password, email);
+        userDAO.createUser(newUser);
+
+        // Create auth token
+        String token = UUID.randomUUID().toString();
+        var auth = new AuthData(token, username);
+        authDAO.createAuth(auth);
+
+        return auth;
+    }
 }
