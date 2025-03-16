b33376622138e34e633b7c3aa55641aebc36a179
StevenChengCCC <chengssh@byu.edu>
2025-03-16 15:11:28 -0600

    create user

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,11 +1,25 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class MySQLUserDAO implements UserDAO{
     @Override
     public void createUser(UserData user) throws DataAccessException {
-
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+            statement.setString(1, user.username());
+            statement.setString(2, hashedPassword);
+            statement.setString(3, user.email());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new DataAccessException("Error creating user: " + e.getMessage());
+        }
     }
 
     @Override
