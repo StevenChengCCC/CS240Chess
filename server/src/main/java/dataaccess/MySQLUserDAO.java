134b75084f2ae1424787fb6eabb28880daa4050d
StevenChengCCC <chengssh@byu.edu>
2025-03-16 15:12:25 -0600

    delete  user

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -5,6 +5,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MySQLUserDAO implements UserDAO{
     @Override
@@ -24,6 +25,20 @@ public class MySQLUserDAO implements UserDAO{
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        String sql = "SELECT username, password, email FROM users WHERE username = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                String dbUsername = rs.getString("username");
+                String hashedPassword = rs.getString("password");
+                String email = rs.getString("email");
+                return new UserData(dbUsername, hashedPassword, email);
+            }
+            return null; // User not found
+        } catch (Exception e) {
+            throw new DataAccessException("Error getting user: " + e.getMessage());
+        }
     }
 }
