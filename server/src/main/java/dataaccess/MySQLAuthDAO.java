a878fc76c6cff6f07b3712b3afe443dbfdeacb2c
StevenChengCCC <chengssh@byu.edu>
2025-03-16 15:05:46 -0600

    createAuth

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -2,11 +2,21 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 public class MySQLAuthDAO implements AuthDAO{
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
-
+        String sql = "INSERT INTO auths (auth_token, username) VALUES (?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement Statement = conn.prepareStatement(sql)) {
+            Statement.setString(1, auth.authToken());
+            Statement.setString(2, auth.username());
+            Statement.executeUpdate();
+        } catch (Exception e) {
+            throw new DataAccessException("Error creating auth: " + e.getMessage());
+        }
     }
 
     @Override
